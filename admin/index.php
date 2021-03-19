1ed059bdd787707466b0c83b0363127d3dd73d95
excelfp <excelfarhanpangestu@yahoo.com>
2021-03-19 16:44:59 +0700

    adjust home img

--- admin/index.php
+++ admin/index.php
@@ -6,12 +6,17 @@
 <div style="height:50px;"></div>
 <div id="page-wrapper">
 <div class="container-fluid">
-<div style="height:40px;"></div>
-	<img src="../upload/cover.jpg" class="thumbnail" style="height:430px; width:1030px;">
+<div style="height:50px;"></div>
+	<img src="../upload/cover.jpg" class="thumbnail" style="height:430px; width:950px;">
 </div>
 </div>
 </div>
 <?php include('script.php'); ?>
 <?php include('modal.php'); ?>
 </body>
+<style type="text/css">
+	body{
+		overflow-x: auto;
+	}
+</style>
 </html>
