02517623350c7e0b1a6ec4cc7629d11ad4e5acc8
excelfp <excelfarhanpangestu@yahoo.com>
2021-07-11 00:04:49 +0700

    update checkout screen
    - product qty

--- user/fetch_checkout.php
+++ user/fetch_checkout.php
@@ -6,7 +6,6 @@
 			<thead>
 				<th></th>
 				<th>Product Name</th>
-				<th>Available Qty</th>
 				<th>Product Price</th>
 				<th>Purchase Qty</th>
 				<th>SubTotal</th>
@@ -21,13 +20,12 @@
 					<tr>
 						<td style="width: 100px;"><button type="button" class="btn btn-danger btn-sm remove_prod" value="<?php echo $row['productid']; ?>"><i class="fa fa-trash fa-fw"></i> Remove</button></td>
 						<td><?php echo $row['product_name']; ?></td>
-						<td><?php echo $row['product_qty']; ?></td>
 						<td align="right"><?php echo number_format($row['product_price'],2); ?></td>
 						<td><button type="button" class="btn btn-warning btn-sm minus_qty2" value="<?php echo $row['productid']; ?>"><i class="fa fa-minus fa-fw"></i></button> 
 							<button type="button"class="btn btn-primary btn-sm add_qty2" value="<?php echo $row['productid']; ?>"><i class="fa fa-plus fa-fw"></i></button> 
 							<?php echo $row['qty'];?>
 						</td>
-						<td align="right"><strong><span class="subt">
+						<td colspan="2" align="right"><strong><span class="subt">
 							<?php
 								$subtotal=$row['qty']*$row['product_price'];
 								echo number_format($subtotal,2);
