4a98597a76264a98ab4bfd5a2a3e424dc791def3
excelfp <excelfarhanpangestu@yahoo.com>
2021-08-23 10:15:06 +0700

    datepicker style css

--- admin/proses_apriori.php
+++ admin/proses_apriori.php
@@ -11,6 +11,25 @@ include('display_mining.php');
 // include_once "mining.php";
 // include_once "display_mining.php";
 ?>
+<head>
+   <!-- bootstrap & fontawesome -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+    <!-- page specific plugin styles -->
+    
+    <!-- page specific plugin styles -->
+    <link rel="stylesheet" href="assets/css/jquery-ui.custom.min.css" />
+    <link rel="stylesheet" href="assets/css/chosen.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker3.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-timepicker.min.css" />
+    <link rel="stylesheet" href="assets/css/daterangepicker.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-colorpicker.min.css" />
+
+    <!-- text fonts -->
+    <link rel="stylesheet" href="assets/css/fonts.googleapis.com.css" />
+</head>
 <div id="wrapper">
 <?php 
 include('navbar.php');
